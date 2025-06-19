28f3a85f30bedd9bd67623da7621fdbacf84b36f
Djeumeni Patchepia Arthur Samuel <djeumenipatchepiaarthursamuel@gmail.com>
2025-06-19 14:04:20 +0100

    MYQL Connection to App

--- app.js
+++ app.js
@@ -13,10 +13,26 @@ app.use(cors())
 
 /* app.get("/",(req,res)=>{
     res.render("index")
-}) */
+}) */ 
 
+    // Initialiszing the server port
 const port = process.env.PORT ||  3002;
 app.listen(port, (req,res)=>{
     console.log(`Server Started on port ${port}`)
 })
-const db = mysql.connect()
+
+// Initializing the MYSQL Data Base
+const db = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    database: 'We-Chat'
+})
+
+// Connecting tjo the Data Base
+db.connect((err)=>{
+    if(err){
+        // Displays the error message with the error variable in case of DB connection error
+        console.error("Erreur de connexion MYSQL: ".err.stack)
+    }
+     console.log("MYSQL Connecté !")
+})
