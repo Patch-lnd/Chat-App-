107419dd745f4e87f1517f86383bfb53922d2c51
Djeumeni Patchepia Arthur Samuel <djeumenipatchepiaarthursamuel@gmail.com>
2025-06-21 22:05:56 +0100

    Correcting the user model code.
    From mongoose to Mysql and Validator

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,4 @@
-/* const userModel = require("../models/userModel.js") */
+const userModel = require("../models/userModel.js") 
 const bcrypt =  require("bcryptjs")
 const validator =  require("validator")
 const jwt =  require("jsonwebtoken")
@@ -13,6 +13,9 @@ const register = async(req,res)=>{
     // Using validator Package
     if(!validator.isEmail(email)) return res.status(400).json("email must be valid")
     if(!validator.isStrongPassword(password)) return res.status(400).json("The password is weak")
+
+    user = "INSERT ".userModel({name,email,password})
+    
 }
 // We included "register" in an Object when exporting since we will ad other exports like login, ect
 module.exports = {register}
--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,44 @@ const mysql = require("mysql")
 const db = require("../db")
 const validator = require("validator")
 
-// Using Schema to shape our documents
+function userModel(data) {
+    const errors = {};
+
+    // Validate name
+    if (!data.name || typeof data.name !== 'string') {
+        errors.name = 'Name is required and must be a string.';
+    } else if (data.name.length < 3 || data.name.length > 30) {
+        errors.name = 'Name must be between 3 and 30 characters.';
+    }
+
+    // Validate email
+    if (!data.email || typeof data.email !== 'string') {
+        errors.email = 'Email is required and must be a string.';
+    } else if (!validator.isEmail(data.email)) {
+        errors.email = 'Email is not valid.';
+    } else if (data.email.length < 4 || data.email.length > 200) {
+        errors.email = 'Email must be between 4 and 200 characters.';
+    }
+
+    // Validate password
+    if (!data.password || typeof data.password !== 'string') {
+        errors.password = 'Password is required and must be a string.';
+    }
+
+    const isValid = Object.keys(errors).length === 0;
+
+    return { isValid, errors };
+}
+
+module.exports = userModel;
+
+
+
+
+
+
+
+/* // Using Schema to shape our documents
 // We use the keyword "new" because this is a class and we use the keyword+Class to creat an Object
 const userSchema = new mysql.schema({
     name: {type: String, required: true, minlength: 3, maxlength: 30},
@@ -17,7 +54,7 @@ const userSchema = new mysql.schema({
 // The first parameter is the name of our collection (start with Cap letter)
 // The second parameter is the variable for the schema
 const userModel = mysql.model("User", userSchema)
-module.exports = userModel;
+module.exports = userModel; */
 
 /* 
     const mysql = require("mysql");
