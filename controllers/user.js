c9acc313a56b978348a028266e86211915a601e9
Djeumeni Patchepia Arthur Samuel <djeumenipatchepiaarthursamuel@gmail.com>
2025-06-28 14:28:03 +0100

    Completing the user input verification
    Now begining with the JWT, created secret key in .env

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,12 @@ const userModel = require("../models/userModel.js")
 const bcrypt =  require("bcryptjs")
 const validator =  require("validator")
 const jwt =  require("jsonwebtoken")
+//const { use } = require("react")
+
+const creatToken = __dirname;
+
+
+
 // As I am using "await" need to ass "async" before the fonction containing it
 const register = async(req,res)=>{
     // Extracting the desired data from the html form in the body 
@@ -14,8 +20,14 @@ const register = async(req,res)=>{
     if(!validator.isEmail(email)) return res.status(400).json("email must be valid")
     if(!validator.isStrongPassword(password)) return res.status(400).json("The password is weak")
 
+    // Saving nuw User
+    //user = "INSERT ".userModel({name,email,password})
+
+    const salt = await bcrypt.genSalt(10)
+   // user.password = await bcrypt.hash(user.password, salt);
+   password = await bcrypt.hash(password, salt);
+      // Saving nuw User
     user = "INSERT ".userModel({name,email,password})
-    
 }
 // We included "register" in an Object when exporting since we will ad other exports like login, ect
 module.exports = {register}
